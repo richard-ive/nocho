227e923ec527d6cab693bfe7fe9f7d87c4820316
Richard Ive <richard.ive.88@gmail.com>
2016-05-21 19:08:21 +0100

    restructure to mode configure to conf file

---
+++ conf/conf.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.CLIENT_ID = "amzn1.application-oa2-client.xxxxxxxxxxxxxx";
+config.CLIENT_SECRET = "xxxxxxxxxxxx";
+config.REDIRECT = "http://xxxxxxxxxxx/authresponse";
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-var express = require('express');
-var app = express();
-var auth = require('./lib/login');
-var alexa = require('./lib/alexa');
-var record = require('node-record-lpcm16');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const auth = require('./lib/login');
+const alexa = require('./lib/alexa');
+const record = require('node-record-lpcm16');
+const fs = require('fs');
+const conf = require('./conf/conf');
 
-
-const CLIENT_ID = "amzn1.application-oa2-client.260a064eafa04546975feb9ac1e557c6";
-const CLIENT_SECRET = "0b797e70c68504f49fdf34bbbe1901e3db200d0c77f90487dc22b04170488fd7";
-const REDIRECT = "http://richard-ive.ddns.net:5006/authresponse";
 const SCOPE_DATA = JSON.stringify({"alexa:all":{"productID":"my_device","productInstanceAttributes":{"deviceSerialNumber":"12345"}}});
 
 app.get('/login', (req, res) => {
   if(!auth.isLoggedIn()){
-    res.redirect(`https://www.amazon.com/ap/oa?client_id=${CLIENT_ID}&scope=alexa:all&scope_data=${SCOPE_DATA}&response_type=code&redirect_uri=${REDIRECT}`);    
+    res.redirect(`https://www.amazon.com/ap/oa?client_id=${conf.CLIENT_ID}&scope=alexa:all&scope_data=${SCOPE_DATA}&response_type=code&redirect_uri=${conf.REDIRECT}`);    
   }else{
     res.end("You're already logged in my friend!");
   }
